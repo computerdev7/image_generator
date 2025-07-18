eacb69e13ebaf8b2e154a5c262f4fcbe9efe4c43
Akash Upadhyay <upadhyayakash687@gmail.com>
2025-07-18 10:12:10 +0530

    Update imageController.js

--- backend/controller/imageController.js
+++ backend/controller/imageController.js
@@ -3,7 +3,7 @@ import { GoogleGenAI, Modality } from "@google/genai";
 import imageSchema from "../models/imageModel.js"
 import path from "path";
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyB2aSTV-lFPdnZ2L7jSP3Jj-90iCkRn9_k" });
+const ai = new GoogleGenAI({ apiKey: process.env.API });
 
 let __dirname = path.resolve()
 
